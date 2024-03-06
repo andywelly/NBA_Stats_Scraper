6cebfaf6204145481c8ed1d84dd1c16708a9729c
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-03-06 18:09:17 +1100

    Added stats per player

--- NBA_Scraper.py
+++ NBA_Scraper.py
@@ -4,19 +4,8 @@ pd.set_option('display.max_columns', None)
 import time
 import numpy as np
 
-test_url = 'https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2023-24&SeasonType=Regular%20Season&StatCategory=PTS'
+stats_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&ISTRound=&LastNGames=10&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2023-24&SeasonSegment=&SeasonType=Regular%20Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight='
 
-r = requests.get(url=test_url).json()
+r = requests.get(url=stats_url).json()
 
-games_url = 'https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json'
-
-games_data = requests.get(url=games_url).json()
-
-games_list = games_data['scoreboard']['games']
-
-for game in games_list:
-    home_team_name = game['homeTeam']['teamName']
-    away_team_name = game['awayTeam']['teamName']
-    game_time_et = game['gameEt']
-    
-    print(f"{home_team_name} V {away_team_name} at {game_time_et}")
+print(r)
