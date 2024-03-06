3f8e9c104c87f46ff82e38dc55dfe6ff36f029ee
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-03-07 02:11:14 +1100

    Fixed float division by 0 error when hit rate is 0

--- odds.py
+++ odds.py
@@ -4,7 +4,7 @@ def point_odds(point_array):
     for market in markets.POINT_MARKETS:
         print("Market: " + str(market) + "+ Points")
         winning_odds = check_market_hit(point_array, market)
-        print("Minimum Winning Odds" + str(winning_odds) + "\n")
+        print("Minimum Winning Odds: " + str(winning_odds) + "\n")
     
         
             
@@ -13,6 +13,9 @@ def check_market_hit(stat_array, market):
     for stat in stat_array:
         if (int(stat) >= market):
             count += 1
-    hit_rate = (count / 5)
-    odds = 1 / hit_rate
-    return odds
+    if count > 0:
+        hit_rate = (count / 5)
+        odds = 1 / hit_rate
+        return odds
+    else:
+        return "N/A - no last 5 hits"
