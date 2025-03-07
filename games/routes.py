3afd98074a8309d67d087e619c876ca9672fc3e9
Andwele Ancheta <andwele.ancheta@gmail.com>
2025-03-08 08:19:09 +1100

    Revert "Move to individual files"
    This reverts commit b07741eac20cce5f73c2ac7b1045c015669877fb.

--- games/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-# Create a Blueprint for games routes and specify the template folder
-games_bp = Blueprint("games", __name__, template_folder="templates")
-
-@games_bp.route("/")
-def games():
-    return render_template("games.html")  # Look for games.html in the games/templates folder
